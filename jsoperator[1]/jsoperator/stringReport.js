ad3425ef82f5245476492520bb76e47ca1afc21d
ap-bit <abhishek205parmar@gmail.com>
2025-11-19 08:43:13 +0530

    Delete jsoperator[1] directory

--- jsoperator[1]/jsoperator/stringReport.js
+++
@@ -1,5 +0,0 @@
-let name1 = " wireless headphones PRO "
-let cleaned = name1.trim().toLowerCase().split(" ").map(w => w.charAt(0).toUpperCase() + w.slice(1)).join(" ")
-cleaned = cleaned.replace("Pro", "Pro Edition")
-console.log(cleaned)
-console.log(cleaned.length)
